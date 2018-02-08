73e77d99ee7ee2e6f9f935e015a984a182cadc4a
ryenchen <irwenqiang@gmail.com>
2018-02-08 13:15:13 +0800

    Create wide_deep_tfrecords.py

---
+++ official/wide_deep/wide_deep_tfrecords.py
@@ -0,0 +1,134 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Example code for TensorFlow Wide & Deep Tutorial using tf.estimator API."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import shutil
+import sys
+
+import tensorflow as tf
+import input_data
+_CSV_COLUMNS = [
+    'age', 'workclass', 'fnlwgt', 'education', 'education_num',
+    'marital_status', 'occupation', 'relationship', 'race', 'gender',
+    'capital_gain', 'capital_loss', 'hours_per_week', 'native_country',
+    'income_bracket'
+]
+
+_CSV_COLUMN_DEFAULTS = [[0], [''], [0], [''], [0], [''], [''], [''], [''], [''],
+                        [0], [0], [0], [''], ['']]
+
+
+CATEGORICAL_COLUMNS = [
+    "workclass", "education", "marital_status", "occupation",
+    "relationship", "race", "gender", "native_country"
+]
+CONTINUOUS_COLUMNS = [
+    "age", "education_num", "capital_gain", "capital_loss",
+    "hours_per_week"
+]
+
+LABEL_COLUMN = "label"
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    '--model_dir', type=str, default='E:/codespace/model/census_model',
+    help='Base directory for the model.')
+
+parser.add_argument(
+    '--model_type', type=str, default='wide_deep',
+    help="Valid model types: {'wide', 'deep', 'wide_deep'}.")
+
+parser.add_argument(
+    '--train_epochs', type=int, default=40, help='Number of training epochs.')
+
+parser.add_argument(
+    '--epochs_per_eval', type=int, default=2,
+    help='The number of training epochs to run between evaluations.')
+
+parser.add_argument(
+    '--batch_size', type=int, default=40, help='Number of examples per batch.')
+
+parser.add_argument(
+    '--train_data', type=str, default='E:/codespace/data/census_data/adult.data',
+    help='Path to the training data.')
+
+parser.add_argument(
+    '--test_data', type=str, default='E:/codespace/data/census_data/adult.test',
+    help='Path to the test data.')
+
+_NUM_EXAMPLES = {
+    'train': 32561,
+    'validation': 16281,
+}
+
+def build_estimator(model_dir, model_type):
+  """Build an estimator appropriate for the given model type."""
+  wide_columns, deep_columns = build_model_columns()
+  hidden_units = [100, 75, 50, 25]
+
+  # Create a tf.estimator.RunConfig to ensure the model is run on CPU, which
+  # trains faster than GPU for this model.
+  run_config = tf.estimator.RunConfig().replace(
+      session_config=tf.ConfigProto(device_count={'GPU': 0}))
+
+  if model_type == 'wide':
+    return tf.estimator.LinearClassifier(
+        model_dir=model_dir,
+        feature_columns=wide_columns,
+        config=run_config)
+  elif model_type == 'deep':
+    return tf.estimator.DNNClassifier(
+        model_dir=model_dir,
+        feature_columns=deep_columns,
+        hidden_units=hidden_units,
+        config=run_config)
+  else:
+    return tf.estimator.DNNLinearCombinedClassifier(
+        model_dir=model_dir,
+        linear_feature_columns=wide_columns,
+        dnn_feature_columns=deep_columns,
+        dnn_hidden_units=hidden_units,
+        config=run_config)
+
+def main(unused_argv):
+  # Clean up the model directory if present
+  shutil.rmtree(FLAGS.model_dir, ignore_errors=True)
+  model = build_estimator(FLAGS.model_dir, FLAGS.model_type)
+
+  # Train and evaluate the model every `FLAGS.epochs_per_eval` epochs.
+  for n in range(FLAGS.train_epochs // FLAGS.epochs_per_eval):
+    model.train(input_fn=lambda: input_data.input_fn(
+        tf.estimator.ModeKeys.TRAIN, FLAGS.train_data, FLAGS.batch_size))
+
+    results = model.evaluate(input_fn=lambda: input_data.input_fn(
+        tf.estimator.ModeKeys.EVAL, FLAGS.test_data, FLAGS.batch_size))
+
+    # Display evaluation metrics
+    print('Results at epoch', (n + 1) * FLAGS.epochs_per_eval)
+    print('-' * 60)
+
+    for key in sorted(results):
+      print('%s: %s' % (key, results[key]))
+
+
+if __name__ == '__main__':
+  tf.logging.set_verbosity(tf.logging.INFO)
+  FLAGS, unparsed = parser.parse_known_args()
+  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
